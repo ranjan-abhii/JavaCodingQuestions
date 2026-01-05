4f65bae146a1a58a9df112c8b483b0334608d4e8
ranjan-abhii <engineer_abhii@yahoo.com>
2026-01-05 23:53:27 +0530

    RahulShetty: Java Basics

---
+++ src/RahulShetty/Introduction.java
@@ -0,0 +1,27 @@
+package RahulShetty;
+
+public class Introduction {
+
+	public static void main(String[] args) {
+		
+		//Data types
+		int myNum = 5;
+		String website = "Rahul Shetty Academy";
+		char letter = 'r';
+		double dec = 5.99;
+		boolean myCard = true;
+		
+		System.out.println(myNum);
+		System.out.println(website);
+		System.out.println(letter);
+		System.out.println(dec);
+		System.out.println(myCard);
+		
+		
+		
+		
+		
+		
+	}
+
+}
